8fabc9011bae0ad84c42857eb814939431208086
dakota127 <pboxler@smile.ch>
2021-01-22 12:22:45 +0100

    credentials file added

--- esp_code/swesp_dose_1/sw_credentials.h
+++ esp_code/swesp_dose_1/sw_credentials.h
@@ -2,14 +2,15 @@
 // WiFi and MQTT credentials
 //
 //---------------------------------------
-#define WAN_SSID "LA20-2"
+
+/*#define WAN_SSID "LA20-2"
 #define WAN_PW  "Tv0usJgbkyfG"
 #define MQTT_IP "192.168.1.12"
-/*
+*/
 #define WAN_SSID "P-NETGEAR"
 #define WAN_PW  "hermannelsa"
 #define MQTT_IP "192.168.1.153"
-*/
+
 
 // für dioty.co
 #define dioty_MQTT_USERID "pboxler@smile.ch"
